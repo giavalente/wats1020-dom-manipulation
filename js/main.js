d66fc85e873d50e5cc576ed5dca486300852aab2
valenteg <valenteg@seattleu.edu>
2017-03-06 03:00:08 -0500

    assignment complete, js added

--- js/main.js
+++ js/main.js
@@ -1,47 +1,77 @@
 //////////////////////////////////////////////////
 // WATS1020 Dom Manipulation
-// Custom script goes here.
+// Custom script
 //////////////////////////////////////////////////
 
+// Run script when DOM is ready for JS
 $( document ).ready(function() {
     var userInfo = {
         firstName: 'Jane',
         lastName: 'Doe'
     };
     var voteCounts = {
-        great: 0,
-        greatest: 0,
-        total: 0
+        great: 1,
+        greatest: 1,
+        total: 2
     };
-    // Place all your Javascript code inside this "document ready" function so
-    // it does not run until the DOM is ready for Javascript manipulation.
+  
+    // Listen for clicks on login button
+   $('.btn-sm').on('click', function(event) {
+       console.log(event);
+       var login = '.navbar-form';
+       var info = '.user-info';
+       var targetElement = event.target;
+     // If login is visible, fade out login and replace with user info 
+     // "Login effect"
+     if ($(login).is(':visible')){
+       $(login).hide();
+       $(info).show();
+     // Fill in user info
+       $('.user-fullname').replaceWith(userInfo.firstName, ' ', userInfo.lastName);
+     // Else, fade out user info and replace with login 
+     // "Logout effect"
+         } else { 
+       $(info).hide();
+       $(login).show();
+         }         
+   });
 
-    // TODO: Create a function to listen for clicks on the "login" button.
-    //      1. When a user clicks the "login" button, hide the login
-    //          form elements on the page.
-    //      2. Fill the user's first and last name into `div.user-info`.
-    //      (NOTE: You do not have to perform any validation on the data as
-    //          a base requirement.)
-
-
-    // TODO: Create a function to listen for clicks on all the "View Details"
-    // buttons so that when a user clicks a "View Details" button they see
-    // the content contained in the elements with the class "details" in the
-    // proper part of the screen.
-    //      1. When user clicks a "view details" button, find the parent of that element.
-    //      2. Within that parent, find all the elements that have the class `details`.
-    //      3. Toggle visibility of all the elements within that parent with the class `details`.
-    //      4. Change the text of the "view details" button to read "hide details" so the user
-    //          understands they can hide the text again.
-
-    // TODO: Create a function that listens for clicks on the voting buttons and
-    // looks at the `data-vote` attribute on each button to see what was voted for,
-    // then determines the updated vote breakdown to adjust the progress bars.
-    //      1. Set up an event listener on the buttons with the `vote` class.
-    //      2. When a button is clicked, look at the `data-vote` attribute to determine
-    //          what the user is voting for ("great" or "greatest").
-    //      3. Increment the counter for whichever vote talley is affected.
-    //      4. Determine the respective percentages (out of 100) for each progress bar.
-    //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
-
-});
+    // Listen for clicks on "View Details" buttons 
+    $('.view-details').on('click', function(event) {
+       console.log(event);
+       var targetElement = event.target;
+    // Find the parent of element, within parent find elements with class "details"
+       var container = targetElement.parentElement.parentElement;
+       $(container).find('.details').each(function(index, el) {
+    // If details visible, hide details and change button text to View
+         if ($(el).is(':visible')){
+            $(el).fadeOut();
+            targetElement.innerText = "View Details"
+    // Else, show details and change button text to Hide
+         } else {
+            $(el).fadeIn();
+           targetElement.innerText = "Hide Details"
+         }
+       });                            
+    });
+  
+    // Listen for clicks on voting buttons, increment total counter
+  $('.vote').on('click', function(event) {
+       console.log(event);
+       voteCounts.total++;
+       var targetElement = event.target;
+       var voteGreat = ((voteCounts.great * 100) / voteCounts.total);
+       var voteGreatest = ((voteCounts.greatest * 100) / voteCounts.total);
+     //  If vote element equals great, increment voteCounts for great, calculate percentage total
+     if ($(targetElement).data('vote') === 'great'){
+           voteCounts.great++;
+           $('.great-progress').width(voteGreat + '%');
+     // Else vote element equals greatest, increment voteCounts for greatest, calculate percentage total
+         } else { ($(targetElement).data('vote') === 'greatest')
+           voteCounts.greatest++;
+           $('.greatest-progress').width(voteGreatest + '%');
+         }
+  }); 
+   
+//closes entire function!  
+                });
